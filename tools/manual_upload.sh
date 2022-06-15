18fdbf448c3f0977b074c2a2e5c741ea81e15fd1
Louis.kraak@gov.bc.ca <Louis.kraak@gov.bc.ca>
2022-06-14 22:09:33 -0700

    t

--- tools/manual_upload.sh
+++ tools/manual_upload.sh
@@ -2,13 +2,13 @@
 
 cat > payload.json <<__HERE__
 {
-  "project": "d545b6f9-af11-4e06-8eeb-b3f41bbc6301",
+  "project": "40b600b9-4588-438d-bca5-ea3ab854f389",
   "bom": "$(cat output.xml |base64 -w 0 -)"
 }
 __HERE__
 
-curl -X "PUT" "https://test-nrm-vulnerability-track.apps.silver.devops.gov.bc.ca/api/v1/bom" \
+curl -X "PUT" "https://dev-nrm-vulnerability-track.apps.silver.devops.gov.bc.ca/api/v1/api/v1/bom" \
      -H 'Content-Type: application/json' \
-     -H 'X-Api-Key: 4gf3IVUHkmp5BO0iQv9SW9RgG969qr4H ' \
+     -H 'X-Api-Key: xxx' \
      -d @payload.json \
      --dump-header - 
